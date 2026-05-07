c4e78849f62a09014f3c0f0de777d5f86f8c86d9
Gohshg <gina.terrestrialecology@gmail.com>
2026-05-07 20:31:52 +0800

    Modified main.tf file

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "sctp-ce12-tfstate" # Change this
-    key    = ""  # Change this
+    bucket = "sctp-ce12-tfstate-bucket" # Change this
+    key    = "Gina-NTU-CIE-Mod3-2.tfstate"  # Change this
     region = "ap-southeast-1"
   }
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix =   # Set your bucket name here
+  bucket_prefix = "Gina-NTU-CIE-Mod3-2-s3-bucket"  # Set your bucket name here
 }
