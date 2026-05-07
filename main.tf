f8565293d5b705669ed90ebc5a1b55ad7641e785
Gohshg <gina.terrestrialecology@gmail.com>
2026-05-07 21:25:44 +0800

    Added required providers

--- main.tf
+++ main.tf
@@ -3,6 +3,13 @@ provider "aws" {
 }
 
 terraform {
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  
   backend "s3" {
     bucket = "sctp-ce12-tfstate-bucket"    # Change this
     key    = "Gina-NTU-CIE-Mod3-2.tfstate" # Change this
